3d942a774bc242d60acee1e125d30315fda226d1
Kade <kadeesterline@gmail.com>
2022-07-23 12:59:02 -0700

    changes getStaticProps

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -56,69 +56,59 @@ const Projects = ({
 
         <div className="m-5 p-5">
           <h2 className="text-4xl font-bold pb-4"> Projects </h2>
-          {projects.map(
-            (project: {
-              name: string;
-              description: string;
-              stack: Array<string>;
-              github: string;
-              deployed: string;
-              demo: string;
-              images: Array<string>;
-            }) => (
-              <div className="border-2 p-4 m-4 rounded-md bg-slate-100 dark:bg-slate-200">
-                <h3 className="text-2xl font-bold dark:text-black">
-                  {project.name}
-                </h3>
-                <div className="flex">
+          {projects.map((project) => (
+            <div className="border-2 p-4 m-4 rounded-md bg-slate-100 dark:bg-slate-200">
+              <h3 className="text-2xl font-bold dark:text-black">
+                {project.name}
+              </h3>
+              <div className="flex">
+                <a
+                  className="p-1 m-1 text-slate-500 dark:text-slate-700"
+                  href={project.github}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaGithub />
+                </a>
+                {project.deployed ? (
                   <a
                     className="p-1 m-1 text-slate-500 dark:text-slate-700"
-                    href={project.github}
+                    href={project.deployed}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    <FaGithub />
+                    <FiLink />
                   </a>
-                  {project.deployed ? (
-                    <a
-                      className="p-1 m-1 text-slate-500 dark:text-slate-700"
-                      href={project.deployed}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FiLink />
-                    </a>
-                  ) : null}
-                  {project.demo ? (
-                    <a
-                      className="p-1 m-1 text-slate-500 dark:text-slate-700"
-                      href={project.demo}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <FaYoutube />
-                    </a>
-                  ) : null}
-                </div>
-                <p className="text-lg text-slate-500 dark:text-slate-600">
-                  <em>{project.description}</em>
-                </p>
-                <ul className="flex border-b-2">
-                  {project.stack.map((item) => (
-                    <li className="p-2 text-slate-500 dark:text-slate-400">
-                      {" "}
-                      - {item}
-                    </li>
-                  ))}
-                </ul>
-                <div className="flex justify-evenly p-3">
-                  {project.images.map((image) => (
-                    <Image src={image} width="325" height="200" />
-                  ))}
-                </div>
+                ) : null}
+                {/* {project.demo ? (
+                  <a
+                    className="p-1 m-1 text-slate-500 dark:text-slate-700"
+                    href={project.demo}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <FaYoutube />
+                  </a>
+                ) : null} */}
               </div>
-            )
-          )}
+              <p className="text-lg text-slate-500 dark:text-slate-600">
+                <em>{project.description}</em>
+              </p>
+              <ul className="flex border-b-2">
+                {project.stack.map((item) => (
+                  <li className="p-2 text-slate-500 dark:text-slate-400">
+                    {" "}
+                    - {item}
+                  </li>
+                ))}
+              </ul>
+              <div className="flex justify-evenly p-3">
+                {project.images.map((image) => (
+                  <Image src={image} width="325" height="200" />
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </main>
     </div>
@@ -126,8 +116,54 @@ const Projects = ({
 };
 
 export const getStaticProps = async () => {
-  const res = await fetch(`${server}/api/projects`);
-  const projects = await res.json();
+  // const res = await fetch(`${server}/api/projects`);
+  // const projects = await res.json();
+
+  const data = [
+    {
+      id: 1,
+      name: "Harmony",
+      description:
+        "Harmony is a messaging and collaboration tool similar to discord or slack.",
+      github: "https://github.com/kadeesterline/harmony",
+      // deployed: "",
+      // demo: "",
+      stack: ["React", "Tailwind", "Ruby on Rails", "PostgreSQL", "AWS S3"],
+      images: [
+        "/project-images/harmony-photos/home-screen.png",
+        "/project-images/harmony-photos/room-view.png",
+      ],
+    },
+    {
+      id: 2,
+      name: "Hard Park",
+      description:
+        "Hard Park is a social media platform built with a focus on cars.",
+      github: "https://github.com/kadeesterline/hard-park-front-end",
+      // deployed: "",
+      // demo: "",
+      stack: ["React", "Bootstrap", "Sinatra", "ActiveRecord", "SQLite"],
+      images: [
+        "/project-images/hardpark-photos/home-page.png",
+        "/project-images/hardpark-photos/the-lot.png",
+      ],
+    },
+    {
+      id: 3,
+      name: "KanBan",
+      description: "KanBan is a project management tool similar to trello.",
+      github: "https://github.com/kadeesterline/kanban-project",
+      deployed: "https://kanbanflatiron.herokuapp.com/",
+      // demo: "",
+      stack: ["React", "Tailwind", "Ruby on Rails", "PostgreSQL"],
+      images: [
+        "/project-images/kanban-photos/board-view.png",
+        "/project-images/kanban-photos/home-view.png",
+      ],
+    },
+  ];
+
+  const projects = data;
 
   return {
     props: {
