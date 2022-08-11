d95d4038d6778e1e54477c9423ac0b18252525f0
Kade <kadeesterline@gmail.com>
2022-08-11 13:15:40 -0700

    changes animation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,33 +7,37 @@ import { motion } from "framer-motion";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <motion.div
-      key={router.route}
-      initial="pageInitial"
-      animate="pageAnimate"
-      exit="pageExit"
-      transition={{ duration: 0.5 }}
-      variants={{
-        pageInitial: {
-          opacity: 0.33,
-          // scale: 1.5,
-        },
-        pageAnimate: {
-          opacity: 1,
-          // scale: 1,
-        },
-        pageExit: {
-          opacity: 0,
-          // scale: 0.5,
-        },
-      }}
-    >
-      {/* <ThemeProvider> */}
+    <>
       <Navigation />
-      <Component {...pageProps} />
+      <motion.div
+        key={router.route}
+        initial="pageInitial"
+        animate="pageAnimate"
+        exit="pageExit"
+        transition={{ duration: 0.5 }}
+        variants={{
+          pageInitial: {
+            opacity: 0.33,
+            // scale: 1.5,
+          },
+          pageAnimate: {
+            opacity: 1,
+            // scale: 1,
+          },
+          pageExit: {
+            opacity: 0,
+            // scale: 0.5,
+          },
+        }}
+      >
+        {/* <ThemeProvider> */}
+
+        <Component {...pageProps} />
+
+        {/* </ThemeProvider> */}
+      </motion.div>
       <Footer />
-      {/* </ThemeProvider> */}
-    </motion.div>
+    </>
   );
 }
 
